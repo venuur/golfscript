49034bc427cbf7dc87f0880c7490f6af4909ce83
Carl Morris <carl.morris.world@gmail.com>
2019-05-17 03:19:35 +0800

    Fix lexer and parser tests. Clean old tests.

--- test/test-parser.rkt
+++ test/test-parser.rkt
@@ -1,45 +1,50 @@
 #lang br
-(require golfscript/parser golfscript/tokenizer brag/support rackunit)
+(require "../parser.rkt" "../tokenizer.rkt")
+(require brag/support)
+(require rackunit rackunit/text-ui)
 
-(check-equal?
- (parse-to-datum (apply-tokenizer make-tokenizer "1 1+"))
- '(gs-program
-   (gs-expr (gs-var 1))
-   (gs-expr (gs-var " "))
-   (gs-expr (gs-var 1))
-   (gs-expr (gs-var "+"))))
+(define test-parser
+  (test-suite
+   "Parser"
+   (check-equal?
+    (parse-to-datum (apply-tokenizer make-tokenizer "1 1+"))
+    '(gs-program
+      (gs-var 1)
+      (gs-var | |)
+      (gs-var 1)
+      (gs-var +)))
 
-(check-equal?
- (parse-to-datum (apply-tokenizer make-tokenizer "1\n1+"))
- '(gs-program
-   (gs-expr (gs-var 1))
-   (gs-expr (gs-var 1))
-   (gs-expr (gs-var "+"))))
+   (check-equal?
+    (parse-to-datum (apply-tokenizer make-tokenizer "1\n1+"))
+    '(gs-program
+      (gs-var 1)
+      (gs-var 1)
+      (gs-var +)))
 
-(check-equal?
- (parse-to-datum (apply-tokenizer make-tokenizer "{-}:+"))
- '(gs-program
-   (gs-expr
-    (gs-assignment
-     (gs-expr
+   (check-equal?
+    (parse-to-datum (apply-tokenizer make-tokenizer "{-}:+"))
+    '(gs-program
+      (gs-assignment
+       (gs-block
+        (gs-var -))
+       (gs-var +))))
+
+   (check-equal?
+    (parse-to-datum (apply-tokenizer make-tokenizer "[50]'23'+"))
+    '(gs-program
+      (gs-list "[")
+      (gs-var 50)
+      (gs-list "]")
+      (gs-string "23")
+      (gs-var +)))
+
+   (check-equal?
+    (parse-to-datum (apply-tokenizer make-tokenizer "{#reminder\n+}~"))
+    '(gs-program
       (gs-block
-       (gs-expr (gs-var "-"))))
-     (gs-var "+")))))
+       (gs-comment "reminder\n")
+       (gs-var +))
+      (gs-var ~)))
+   ))
 
-(check-equal?
- (parse-to-datum (apply-tokenizer make-tokenizer "[50]'23'+"))
- '(gs-program
-  (gs-expr (gs-list (gs-expr (gs-var 50))))
-  (gs-expr (gs-string "23"))
-  (gs-expr (gs-var "+"))))
-
-(check-equal?
- (parse-to-datum (apply-tokenizer make-tokenizer #<<HERE
-{#reminder
-+}~
-HERE
-                                  ))
- '(gs-program
-  (gs-expr
-   (gs-block (gs-expr (gs-comment "reminder\n")) (gs-expr (gs-var "+"))))
-  (gs-expr (gs-var "~"))))
+(run-tests test-parser)
